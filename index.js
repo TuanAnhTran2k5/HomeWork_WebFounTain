a603a2d0c40a6fa350867b2ce17e0283070126c2
TuanAnhTran2k5 <tranvantuananh01012005@gmail.com>
2025-05-06 22:36:49 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", function() {
+    
+    const menuToggle = document.getElementById("menu-toggle");
+    const mobileNav = document.getElementById("mobile-nav");
+    const barIcon = document.getElementById("bar-icon");
+    const closeIcon = document.getElementById("close-icon");
+
+    console.log(menuToggle);
+
+    menuToggle.addEventListener("click", function (){
+        
+        mobileNav.classList.toggle("active");
+
+        if(mobileNav.classList.contains("active")){
+            barIcon.style.display = "none";
+            closeIcon.style.display = "block";
+        } else{
+            barIcon.style.display = "block";
+            closeIcon.style.display = "none";
+        }
+
+
+
+
+    });
+});
+
+
